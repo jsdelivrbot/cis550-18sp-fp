d32580d5a5496891b0bfcdb5e0160e4b185f9abf
Derek <kuangda92@gmail.com>
2018-03-06 15:44:51 -0500

    Set up code skeleton

--- http-server/index.js
+++ http-server/index.js
@@ -1,13 +1,45 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var path = require('path');
+var mysql = require('mysql');
+/*-----------------------------------------------------*/
+// Connect to the Database
+var connection = mysql.createConnection({
+  host: 'fling.seas.upenn.edu',
+  user: 'kuangda',
+  password: 'kwbJqcvxrN9Nx',
+  database: "kuangda"
+});
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+connection.connect(function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
+/*-----------------------------------------------------*/
+// Application configuration set
+app.set('port', (process.env.PORT || 5000));
+// var pug = require('pug');
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.use(express.static(__dirname + '/public'));
+/*-----------------------------------------------------*/
+// Router
+// HTML pages in views
+// java script in public/javascript
+app.get('/', function (request, response) {
+  response.sendFile(path.join(__dirname, '/views', 'firstPage.html'));
+});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+app.listen(app.get('port'), function () {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
+// TODO： when to end the connection?
+// connection.end(function (err) {
+//   // The connection is terminated now
+//   console.log("Connection close");
+// });
