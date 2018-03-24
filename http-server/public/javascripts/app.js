ce34f2d1cf49381171a08eba39942f5795b3e244
Fan Chen <37029784+fanchen0608@users.noreply.github.com>
2018-03-23 21:32:15 -0400

    Update app.js

--- http-server/public/javascripts/app.js
+++ http-server/public/javascripts/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('USuniversity', []);
 app.controller('searchController', function($scope, $http) {
   $scope.Submit = function() {
-    var request = $http.get('/secondPage/'+$scope.sat+'&'+$scope.upperLimit+'&'+$scope.lowerLimit);
+    var request = $http.get('/secondpage/'+$scope.sat+'&'+$scope.upperLimit+'&'+$scope.lowerLimit);
     request.success(function(data) {
       $scope.data = data;
     });
@@ -11,7 +11,7 @@ app.controller('searchController', function($scope, $http) {
   };
   $scope.Find = function() {
     $scope.name = $scope.data.univ_name;
-    var request = $http.get('/thirdPage/'+$scope.name);
+    var request = $http.get('/thirdpage/'+$scope.name);
     request.success(function(data) {
       $scope.data2 = data;
     });
