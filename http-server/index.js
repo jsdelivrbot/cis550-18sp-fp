d5f384b86d211dfbe7a05ac8c85fa1bd84ba1af0
yitonglong <yitonglong10212>
2018-04-04 12:34:50 -0400

    show data on secondpage and add link on univ)url

--- http-server/index.js
+++ http-server/index.js
@@ -32,27 +32,40 @@ app.use(express.static(__dirname + '/public'));
 // HTML pages in views
 // java script in public/javascript
 app.get('/', function (request, response) {
-  response.sendFile(path.join(__dirname, '/views', 'firstPage.html'));
+  response.sendFile(path.join(__dirname, '/views', 'firstpage.html'));
 });
 app.get('/app.js', function (request, response) {
   response.sendFile(path.join(__dirname, '/public/javascripts', 'app.js'));
 });
 
-app.get('/app.js', function (request, response) {
-  response.sendFile(path.join(__dirname, '/public/javascripts', 'app.js'));
+app.get('/universitylist',function(request, response) {
+  response.sendFile(path.join(__dirname, '/views','secondpage.html'));
 });
 
-// TODO: test aws query here
-// app.get('/secondpage/:input'){
-//   connection.query({
-//     sql: 'SELECT univ_name, rank, city, adm_rate, sat_avg, url FROM University u NATURAL JOIN admission a WHERE sat_avg < ? AND rank <= ? AND rank >= ?;', values: [parameter]
-//   }, function (err, results, fields) {
-//     console.log("Sent query.");
-//     if (err) throw err;
-//     console.log(results);
-//     response.json(results);
-//   });
-// }
+app.get('/universitylist/:sat/:upperLimit/:lowerLimit', function(request,response){
+  console.log("something here??");
+  var mysql = require('mysql');
+  var connection = mysql.createConnection({
+    host: 'cis550project.cpqrgrawy8bo.us-east-2.rds.amazonaws.com',
+    port: 3306,
+    user: 'cis550project',
+    password: 'wyuVj[x^e.x9i',
+    database: "cis550fp"
+  });
+  var sat=request.params.sat
+  var upperLimit=request.params.upperLimit
+  var lowerLimit=request.params.lowerLimit
+  var query= "SELECT univ_name, rank, city, adm_rate, sat_avg, univ_url FROM cis550fp.University NATURAL JOIN cis550fp.Admission WHERE sat_avg <="+sat+" AND rank <="+lowerLimit+" AND rank >="+upperLimit+" order by rank ASC;";   
+  connection.connect(function(err){
+  if (err) throw err;
+  //if (sat!='undefined' & upperLimit!='undefined' & lowerLimit!='undefined')
+    console.log(query);
+    connection.query(query, function (err, result, fields) {
+    if (err) throw err;
+    response.json(result);
+  })
+})
+})
 
 app.listen(app.get('port'), function () {
   console.log("Node app is running at localhost:" + app.get('port'));
--- http-server/public/javascripts/app.js
+++ http-server/public/javascripts/app.js
@@ -1,15 +1,23 @@
 var app = angular.module('USuniversity', []);
-app.controller('searchController', function($scope, $http) {
+
+app.controller('searchController', function($scope, $http, $window) {
   $scope.Submit = function() {
-    var request = $http.get('/secondpage/'+$scope.sat+'&'+$scope.upperLimit+'&'+$scope.lowerLimit);
-    request.success(function(data) {
-      $scope.data = data;
-    });
-    request.error(function(data) {
-      console.log('err');
-    });
+    $window.sessionStorage.sat=$scope.sat;
+    $window.sessionStorage.upperLimit=$scope.upperLimit;
+    $window.sessionStorage.lowerLimit=$scope.lowerLimit;
+    window.location='/universitylist';
   };
-  $scope.Find = function() {
+});
+
+app.controller('tempController',function($scope, $http, $window){
+  var request=$http.get('/universitylist/'+$window.sessionStorage.sat+'/'+$window.sessionStorage.upperLimit+'/'+$window.sessionStorage.lowerLimit);
+  request.success(function(data){
+    $scope.data=data;
+  });
+  request.error(function(data){
+    console.log('err');
+  });
+  /*scope.Find = function() {
     $scope.name = $scope.data.univ_name;
     var request = $http.get('/thirdpage/'+$scope.name);
     request.success(function(data) {
@@ -18,5 +26,5 @@ app.controller('searchController', function($scope, $http) {
     request.error(function(data) {
       console.log('err');
     });
-  };
+  };*/
 });
